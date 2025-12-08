1e80a6080280e8a9079d32a1e2c9b61335c2b191
sai1505 <vsai1505.bona@gmail.com>
2025-12-08 17:19:09 +0530

    Made some front screen Changes.

--- lib/screens/welcome_screen_page.dart
+++ lib/screens/welcome_screen_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomeScreenPage extends StatelessWidget {
   const WelcomeScreenPage({super.key});
@@ -6,7 +7,49 @@ class WelcomeScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("data")),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(
+                'assets/clearDesk.svg',
+                height: 250,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Do you lose tracking of your own assets ?",
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyLarge?.copyWith(fontSize: 22),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 14,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+                onPressed: () => {},
+                child: Text(
+                  "Next",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyLarge?.copyWith(fontSize: 22),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
