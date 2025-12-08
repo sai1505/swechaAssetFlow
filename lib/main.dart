918b49e2ce229655038eff2498dd2e9b198c9ab4
sai1505 <vsai1505.bona@gmail.com>
2025-12-08 11:08:35 +0530

    Got to know about Theme Data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:swecha_asset_flow/screens/welcome_screen_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Text("data"),
-      ),
+      title: 'SwechaAssetFlow',
+      debugShowCheckedModeBanner: false,
+      home: const WelcomeScreenPage(),
+      themeMode: ThemeMode.system,
+      theme: _lightTheme,
     );
   }
+
+  //LightMode
+  final ThemeData _lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    colorSchemeSeed: const Color.fromARGB(255, 255, 63, 0),
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      backgroundColor: const Color.fromARGB(255, 255, 63, 0),
+      foregroundColor: Colors.white,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+    ),
+    cardTheme: const CardThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(18)),
+      ),
+    ),
+    fontFamily: 'GolosText',
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+      titleMedium: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+      titleSmall: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+
+      bodyLarge: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+      bodyMedium: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
+      bodySmall: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+
+      labelLarge: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+      labelMedium: TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
+      labelSmall: TextStyle(fontWeight: FontWeight.w400, fontSize: 11),
+    ),
+  );
 }
